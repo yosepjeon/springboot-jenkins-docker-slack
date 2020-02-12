b5889623f3f5cc2409e1411e1e26dad5d66e831b
yosepjeon <enekelx1@naver.com>
2020-02-12 15:46:59 +0900

    docker를 활용한 ci 실습 시작

---
+++ src/main/java/com/yosep/ci/YosepSpringCiStartApplication.java
@@ -0,0 +1,13 @@
+package com.yosep.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepSpringCiStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosepSpringCiStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yosep/ci/YosepSpringCiStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosep.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosepSpringCiStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
