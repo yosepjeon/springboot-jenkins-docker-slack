f9dab6ab4c72a094426004e219f8e5f25ce8b77a
yosepjeon <enekelx1@naver.com>
2020-02-12 18:30:09 +0900

    코드 변경사항 발생

--- src/main/java/com/yosep/ci/YosepSpringCiStartApplication.java
+++ src/main/java/com/yosep/ci/YosepSpringCiStartApplication.java
@@ -8,6 +8,7 @@ public class YosepSpringCiStartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YosepSpringCiStartApplication.class, args);
+		System.out.println("HI");
 	}
 
 }
